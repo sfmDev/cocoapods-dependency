7171d7925311648585c7c0451f373d82b87a4ada
fengming.shi <fengming.sfm@alibaba-inc.com>
2019-03-26 13:34:14 +0800

    remove local js file

--- lib/cocoapods-dependency/command/dependency.rb
+++ lib/cocoapods-dependency/command/dependency.rb
@@ -19,6 +19,10 @@ module Pod
         @name = argv.shift_argument
         @dependency_names = Array.new
         @dependencies_hash = Hash.new
+
+        @output_dependencies_hash = Hash.new
+        @output_dependencies_hash["nodes"] = []
+        @output_dependencies_hash["edges"] = []
         super
       end
 
@@ -28,13 +32,8 @@ module Pod
       end
 
       def run
-        require 'yaml'
-        require 'launchy'
-
-        Launchy.open( "/Users/SFM/workspace/cocoapods-dependency/lib/cocoapods-dependency/front-end/dependency_graph.html")
-
         UI.title "Calculating dependencies" do
-          dependencies ## hash
+          dependencies
         end
 
         UI.title 'Dependencies' do
@@ -42,16 +41,11 @@ module Pod
             if dep.is_a? Hash
               dep_hash = dep.to_h
               key = remove_version(dep_hash.keys.first)
-              if !is_subspec(key)
-                @dependencies_hash.store(key, dep_hash[dep_hash.keys.first])
-                @dependency_names << key
-              end
-
+              @dependencies_hash.store(key, dep_hash[dep_hash.keys.first])
+              @dependency_names << key
             else
-              if !is_subspec(dep)
-                @dependencies_hash.store(remove_version(dep), dep)
-                @dependency_names << remove_version(dep)
-              end
+              @dependencies_hash.store(remove_version(dep), dep)
+              @dependency_names << remove_version(dep)
             end
           }
 
@@ -67,32 +61,91 @@ module Pod
           third_party = Array.new
 
           @dependency_names.map { |dep|
-            if dep =~ /^LPD.*?Module/
+            temp_dep = get_pod_name(dep)
+            if temp_dep =~ /^LPD.*?Module/
               module_arr << dep
-            elsif dep =~ /^LPD/ && dep =~ /Kit|SDK|Service/
+            elsif temp_dep =~ /^LPD/ && temp_dep =~ /Kit|SDK|Service/
               kss_arr << dep
-            elsif dep =~ /\||^ELM|^APF|^LPD/
+            elsif temp_dep =~ /\||^ELM|^APF|^LPD/
               second_party << dep
             else
-              third_party << dep
+              if !is_subspec(dep)
+                third_party << dep
+              end
             end
           }
 
-          UI.title 'First Level Dependencies' do
-              puts module_arr
-          end
+          root_sourceId = "LPDTeamiOS"
+          add_root(root_sourceId, module_arr.count)
 
-          UI.title 'Second Level Dependencies' do
-              puts kss_arr
+          UI.title 'Other Level Dependencies' do
+              puts third_party
+              for i in 0..third_party.count-1
+                pod_name = third_party[i]
+                add_node(pod_name, get_pod_size(pod_name), i, 4, i % 2 == 0)
+              end
           end
 
           UI.title 'Third Level Dependencies' do
               puts second_party
+              for i in 0..second_party.count-1
+                pod_name = second_party[i]
+                add_node(second_party[i], get_pod_size(pod_name), i, 3, i % 2 == 0)
+              end
           end
 
-          UI.title 'Other Level Dependencies' do
-              puts third_party
+          UI.title 'Second Level Dependencies' do
+              puts kss_arr
+              for i in 0..kss_arr.count-1
+                pod_name = kss_arr[i]
+                add_node(kss_arr[i], get_pod_size(pod_name), i, 2, i % 2 == 0)
+              end
           end
+
+          UI.title 'First Level Dependencies' do
+              puts module_arr
+              for i in 0..module_arr.count-1
+                pod_name = module_arr[i]
+                add_node(module_arr[i], get_pod_size(pod_name), i, 1, i % 2 == 0)
+                add_edge(root_sourceId, remove_version(module_arr[i]))
+              end
+          end
+
+          @dependencies_hash.each { |key,value|
+            if value.class == Array
+               for i in 0..value.count - 1
+                 pod = value[i]
+                 if key != value[i]
+                   if !is_subspec(remove_version(pod))
+                     add_edge(key, remove_version(pod))
+                   end
+                 end
+               end
+            end
+          }
+
+          p `rm -rf pod_dependency.json`
+          p `touch pod_dependency.json`
+          File.open("./pod_dependency.json","w") do |f|
+            dep_json = JSON @output_dependencies_hash
+            f.write("index(" + dep_json + ")")
+
+            require 'yaml'
+            require 'launchy'
+
+            p `pwd`
+
+            # Launchy.open("/Users/SFM/workspace/cocoapods-dependency/lib/cocoapods-dependency/front-end/dependency_graph.html")
+          end
+        end
+      end
+
+
+      def get_pod_size pod_name
+        if  @dependencies_hash["#{pod_name}"].class == Array
+          return 15
+        else
+          return 15
         end
       end
 
@@ -107,11 +160,73 @@ module Pod
         return @dependencies_hash[pod_name]
       end
 
+      def get_pod_name pod
+        pod_name_subspec = remove_version(pod)
+        if pod_name_subspec.include? "/"
+          return pod_name_subspec.split("/").first
+        end
+        pod_name_subspec
+      end
+
       def is_subspec pod_name
           if pod_name.include? "/"
             return true
           end
   	      return false
+      end
+
+      def add_root(pod_name, size)
+          node = Hash.new
+          node["color"] = "#4f19c7"
+          node["label"] = pod_name
+          node["attributes"] = {}
+          node["y"] = -2000
+          node["x"] = 0
+          node["id"] = pod_name
+          node["size"] = size
+          @output_dependencies_hash["nodes"] << (node)
+      end
+
+      def get_level_color level
+          if level == 0
+            return "#F75855"
+          elsif level == 1
+            return "#EC944B"
+          elsif level == 2
+            return "#ACD543"
+          elsif level == 3
+            return "#45D5AA"
+          elsif level == 4
+            return "#EB1E0F"
+          else
+            return "#45D5AA"
+          end
+      end
+
+      def add_node(pod_name, size, index, level, is_left)
+          node = Hash.new
+          node["color"] = get_level_color(level)
+          node["label"] = pod_name
+          node["attributes"] = {}
+          node["y"] = -2000 + level * 2000
+          if is_left
+            node["x"] = 0 + (index + 1) * 100
+          else
+            node["x"] = 0 - index * 100
+          end
+
+          node["id"] = pod_name
+          node["size"] = size
+          @output_dependencies_hash["nodes"] << (node)
+      end
+
+      def add_edge(source_id, target_id)
+          edge = Hash.new
+          edge["sourceID"] = source_id
+          edge["targetID"] = target_id
+          edge["attributes"] = {}
+          edge["size"] = 1
+          @output_dependencies_hash["edges"] << (edge)
       end
 
       def dependencies
