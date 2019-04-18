b5e87fc62ec878c807fd52aa3101d9ab54669e50
fengming.shi <fengming.sfm@alibaba-inc.com>
2019-04-18 17:03:48 +0800

    [CHG] finish config json

--- lib/cocoapods-dependency/command/dependency.rb
+++ lib/cocoapods-dependency/command/dependency.rb
@@ -15,10 +15,25 @@ module Pod
         ].concat(super)
       end
 
+      def self.options
+          [
+            ['--use-tree', 'Whether the equal level dependencies use tree'],
+          ].concat(super)
+        end
+
       def initialize(argv)
         @name = argv.shift_argument
+
+        @use_tree = argv.flag?('use-tree', false)
+        @dependencies_regular_hash = Hash.new
+
+        read_config
+
         @dependency_names = Array.new
         @dependencies_hash = Hash.new
+        @dependencies_tree_levels = Array.new
+        @dependencies_level_hash = Hash.new
+
 
         @output_dependencies_hash = Hash.new
         @output_dependencies_hash["nodes"] = []
@@ -29,6 +44,20 @@ module Pod
       def validate!
         super
         puts @name
+      end
+
+      def read_config
+        if File::exist?("./dependency_config")
+          file = File.open("./dependency_config", 'r')
+          @dependencies_regular_hash = JSON.load(file.read())
+          if !@dependencies_regular_hash
+            puts "dependency_config file cannot be converted to JSONObject"
+            return
+          end
+        else
+          puts "Please add json file named dependency_config in project root path"
+          return
+        end
       end
 
       def run
@@ -55,60 +84,103 @@ module Pod
             end
           end
 
-          module_arr = Array.new
-          kss_arr = Array.new
-          second_party = Array.new
-          third_party = Array.new
+          modules_arr = Array.new
+          @dependencies_regular_hash.each {
+	           modules_arr << []
+          }
+          modules_arr << []
 
           @dependency_names.map { |dep|
-            temp_dep = get_pod_name(dep)
-            if temp_dep =~ /^LPD.*?Module/
-              module_arr << dep
-            elsif temp_dep =~ /^LPD/ && temp_dep =~ /Kit|SDK|Service/
-              kss_arr << dep
-            elsif temp_dep =~ /\||^ELM|^APF|^LPD/
-              second_party << dep
-            else
+          	temp_dep = get_pod_name(dep)
+          	has_add = false
+          	for i in 0..@dependencies_regular_hash.keys.count-1
+          	  key = @dependencies_regular_hash.keys[i]
+          	  regular_expression = @dependencies_regular_hash[key]
+          		if temp_dep =~ /#{regular_expression}/
+          		  	modules_arr[i] << dep
+          			has_add = true
+          			break
+          		end
+          	end
+          	if !has_add
               if !is_subspec(dep)
-                third_party << dep
+                modules_arr.last << dep
               end
-            end
+          	end
           }
 
+          p modules_arr
+
+          if @use_tree
+            for arr in modules_arr
+              split_modoule_with_tree(arr)
+            end
+          end
+
           root_sourceId = get_project_name
-          add_root(root_sourceId, module_arr.count)
+          add_root(root_sourceId, modules_arr[0].count)
 
-          UI.title 'Other Level Dependencies' do
-              puts third_party
-              for i in 0..third_party.count-1
-                pod_name = third_party[i]
-                add_node(pod_name, get_pod_size(pod_name), i, 4, i % 2 == 0)
+          if @use_tree
+            for i in 0..@dependencies_tree_levels.count - 1
+              ## i == level
+              for j in 0..@dependencies_tree_levels[i].count-1
+                ## j == pod_name
+                pod_name = @dependencies_tree_levels[i][j]
+                if pod_name != nil || pod_name.class == String || pod_name.length > 0
+                  add_tree_node(pod_name, get_pod_size(pod_name), j, i, j % 2 == 0)
+                end
               end
-          end
+            end
 
-          UI.title 'Third Level Dependencies' do
-              puts second_party
-              for i in 0..second_party.count-1
-                pod_name = second_party[i]
-                add_node(second_party[i], get_pod_size(pod_name), i, 3, i % 2 == 0)
-              end
-          end
+            ## add root -> first level egde
+            for pod_name in modules_arr[0]
+              add_edge(root_sourceId, remove_version(pod_name))
+            end
 
-          UI.title 'Second Level Dependencies' do
-              puts kss_arr
-              for i in 0..kss_arr.count-1
-                pod_name = kss_arr[i]
-                add_node(kss_arr[i], get_pod_size(pod_name), i, 2, i % 2 == 0)
+          else
+            for j in 0..modules_arr.count-1
+              pod_arr = modules_arr[j]
+              for i in 0..pod_arr.count-1
+                 pod_name = pod_arr[i]
+                 add_node(pod_arr[i], get_pod_size(pod_name), i, j+1, i % 2 == 0)
+                 if j == 0
+                   add_edge(root_sourceId, remove_version(pod_arr[i]))
+                 end
               end
-          end
+            end
 
-          UI.title 'First Level Dependencies' do
-              puts module_arr
-              for i in 0..module_arr.count-1
-                pod_name = module_arr[i]
-                add_node(module_arr[i], get_pod_size(pod_name), i, 1, i % 2 == 0)
-                add_edge(root_sourceId, remove_version(module_arr[i]))
-              end
+            # UI.title 'First Level Dependencies' do
+            #     puts module_arr
+            #     for i in 0..module_arr.count-1
+            #       pod_name = module_arr[i]
+            #       add_node(module_arr[i], get_pod_size(pod_name), i, 1, i % 2 == 0)
+            #       add_edge(root_sourceId, remove_version(module_arr[i]))
+            #     end
+            # end
+            #
+            # UI.title 'Second Level Dependencies' do
+            #     puts kss_arr
+            #     for i in 0..kss_arr.count-1
+            #       pod_name = kss_arr[i]
+            #       add_node(kss_arr[i], get_pod_size(pod_name), i, 2, i % 2 == 0)
+            #     end
+            # end
+            #
+            # UI.title 'Third Level Dependencies' do
+            #     puts second_party
+            #     for i in 0..second_party.count-1
+            #       pod_name = second_party[i]
+            #       add_node(second_party[i], get_pod_size(pod_name), i, 3, i % 2 == 0)
+            #     end
+            # end
+            #
+            # UI.title 'Other Level Dependencies' do
+            #     puts third_party
+            #     for i in 0..third_party.count-1
+            #       pod_name = third_party[i]
+            #       add_node(pod_name, get_pod_size(pod_name), i, 4, i % 2 == 0)
+            #     end
+            # end
           end
 
           @dependencies_hash.each { |key,value|
@@ -116,9 +188,7 @@ module Pod
                for i in 0..value.count - 1
                  pod = value[i]
                  if key != value[i]
-                   if !is_subspec(remove_version(pod))
-                     add_edge(key, remove_version(pod))
-                   end
+                   add_edge(key, remove_version(pod))
                  end
                end
             end
@@ -132,10 +202,76 @@ module Pod
 
             require 'yaml'
             require 'launchy'
+            p `rm -rf dependency_graph.html`
             p `wget https://raw.githubusercontent.com/sfmDev/cocoapods-dependency/master/lib/cocoapods-dependency/front-end/dependency_graph.html`
             Launchy.open("./dependency_graph.html")
           end
         end
+      end
+
+      def split_modoule_with_tree modules
+      	for pod in modules
+      	   dep_array = @dependencies_hash[pod]
+
+      	   if dep_array.is_a? Array
+             dep_array.map! { |pod_name|
+               remove_version(pod_name)
+             }
+      	     for i in 0..dep_array.count-1
+      	       if modules.include?(dep_array[i])
+      	          record_each_be_dependencied(dep_array[i])
+      	       end
+      	     end
+      	   end
+      	end
+
+        temp_modules_dep = @dependencies_level_hash.keys.flatten
+        first_level_arr = modules - temp_modules_dep
+
+        @dependencies_tree_levels << first_level_arr
+
+        clean_record
+        if temp_modules_dep.count > 0
+          split_modoule_with_tree(temp_modules_dep)
+        end
+      end
+
+      def clean_record
+        @dependencies_level_hash.clear
+      end
+
+      def record_each_be_dependencied pod_name
+        if @dependencies_level_hash[pod_name]
+          @dependencies_level_hash[pod_name] = 2
+        else
+          @dependencies_level_hash[pod_name] = 1
+        end
+      end
+
+      def get_pod_level pod_name
+        if @dependencies_level_hash[pod_name]
+          return @dependencies_level_hash[pod_name]
+        else
+          return 1
+        end
+      end
+
+      def sperate_modules_with_level module_names
+        pod_sperate_arr = Array.new
+        pod_sperate_hash = Hash.new
+        for pod in module_names
+          level = get_pod_level(pod)
+          current_pods = pod_sperate_hash[level]
+          current_pods = current_pods.to_s + "," + pod
+          pod_sperate_hash[level] = current_pods
+        end
+
+        pod_sperate_hash.each { |key,value|
+          index = key
+          pods = value.split(",")
+          pod_sperate_arr[index] = pods.delete_if { |item| item == nil || item.class != String || item.length == 0}
+        }
+        return pod_sperate_arr
       end
 
       def get_project_name
@@ -144,10 +280,14 @@ module Pod
       end
 
       def get_pod_size pod_name
-        if  @dependencies_hash["#{pod_name}"].class == Array
+        if @use_tree
           return 15
         else
-          return 15
+          if  @dependencies_hash["#{pod_name}"].class == Array
+            return 10
+          else
+            return 10
+          end
         end
       end
 
@@ -183,10 +323,14 @@ module Pod
           node["label"] = pod_name
           node["attributes"] = {}
           node["y"] = -2000
-          node["x"] = 0
+          node["x"] = 200
           node["id"] = pod_name
           node["size"] = size
           @output_dependencies_hash["nodes"] << (node)
+      end
+
+      def get_tree_level_color pod_name
+
       end
 
       def get_level_color level
@@ -205,16 +349,34 @@ module Pod
           end
       end
 
+      def add_tree_node(pod_name, size, index, depth, is_left)
+          node = Hash.new
+          node["color"] = get_level_color(depth)
+          node["label"] = pod_name
+          node["attributes"] = {}
+          node["y"] = -2000 + (depth + 1) * 1500
+
+          if is_left
+            node["x"] = 0 + (index + 1) * 400
+          else
+            node["x"] = 0 - index * 400
+          end
+
+          node["id"] = pod_name
+          node["size"] = size
+          @output_dependencies_hash["nodes"] << (node)
+      end
+
       def add_node(pod_name, size, index, level, is_left)
           node = Hash.new
           node["color"] = get_level_color(level)
           node["label"] = pod_name
           node["attributes"] = {}
-          node["y"] = -2000 + level * 2000
+          node["y"] = -2000 + level * 3000
           if is_left
-            node["x"] = 0 + (index + 1) * 150
+            node["x"] = 0 + (index + 1) * 200
           else
-            node["x"] = 0 - index * 150
+            node["x"] = 0 - index * 200
           end
 
           node["id"] = pod_name
